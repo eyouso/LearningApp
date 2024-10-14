3227654afcb8051919f2b36a0970ce40ab366d1e
Ethan <ethanyouso@gmail.com>
2024-10-14 14:03:29 -0500

    .env file configuration

--- frontend/App.js
+++ frontend/App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
-import { View } from 'react-native';
 
 export default function App() {
   return <AppNavigator />;
--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -2,14 +2,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';  // For auth persistence
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA221lDsRaItItCvxRXBoHVMpfF3FQBix0",
-  authDomain: "learningapp-2e78b.firebaseapp.com",
-  projectId: "learningapp-2e78b",
-  storageBucket: "learningapp-2e78b.appspot.com",
-  messagingSenderId: "986996563100",
-  appId: "1:986996563100:web:9eaf7a165a3ed72542e159"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
 // Initialize Firebase
