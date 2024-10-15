9651fb4434b33508828a8722794d01dca90b7cd5
Ethan <ethanyouso@gmail.com>
2024-10-15 10:21:56 -0500

    before testing

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -3,10 +3,17 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      ["module:react-native-dotenv", {
-        moduleName: "@env",
-        path: ".env",
-      }]
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
+      "@babel/plugin-transform-optional-chaining",
+      "@babel/plugin-transform-object-rest-spread",
+      "@babel/plugin-transform-nullish-coalescing-operator",
+      "@babel/plugin-transform-class-properties",
     ],
   };
 };
